5e1bdcf56201c1ac2e190c4e54a0bf0768598d8b
stuart-rickard <stuart@bau-dev.com>
2021-11-19 16:52:54 -0800

    working but has a few bugs and needs to be refactors to break out some functions

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 const numbersZeroToNine = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_'];
 
-// objects and array of these objects to allow "for" loops to be used for creating the password
+// character type objects
 var lowerCaseLettersObject = {
   name: "lower case letter",
   includeInPassword: false,
@@ -26,19 +26,18 @@ var specialCharactersObject = {
   includeInPassword: false,
   arrayOfCharacters: specialCharacters
 }
+//array of character-type objects; this allows "for" loops to be used
 var passwordSource = [lowerCaseLettersObject, upperCaseLettersObject, numbersZeroToNineObject, specialCharactersObject];
 
 var passwordLength = false; // false is a placeholder until user enters a valid password length; while it is false the user is prompted for a password length
 var numberOfTypesOfCharacters = 0; // used to confirm that user has selected at least one type of character and to reserve space for required characters
 var characterSet = []; // working array to use as source for password characters
-var passwordArray = ["t", "e", "s", "t"]; // TODO make empty later // working array to hold randomly-selected password characters
-var passwordString = ""; // this is the string for the outputted password
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var passwordArray = []; // working array to hold the password characters as they are generated
+var passwordString = ""; // this is the string for the password output
 
 var resetVariables = function() {
-  passwordLength = 10;  //reset to false
+  passwordLength = false;
+  numberOfTypesOfCharacters = 0;
   characterSet = [];
   passwordArray = [];
   passwordString = "";
@@ -46,6 +45,9 @@ var resetVariables = function() {
     passwordSource[i].includeInPassword = false;
   }
 }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // generates random number between two numbers, inclusive
 var getRandomNumberFromTo = function(min, max) {
@@ -60,7 +62,7 @@ var getRandomElementFromArray = function(array) {
   return array[randomIndex];
 }
 
-// splice in random location
+// splices a given value in a random location in an array; this is used to make sure that required characters are always included in the output
 var spliceValueAtRandomIndex = function(value, array) {
   var lastElementIndex = array.length - 1;
   var randomIndex = getRandomNumberFromTo(0, lastElementIndex);
@@ -97,22 +99,7 @@ var validateEntryNumberWithMinMax = function(string, min, max) {
 
 // TODO add response to cancel choice
 
-                                  // using the typesOfCharactersToInclude global variable arraty, validates at least one character type has been included for the password generator
-                                  // var atLeastOneCharacterTypeSelected = function() {
-                                  //   if (typesOfCharactersToInclude.includes(true)) {
-                                  //     return true;
-                                  //   }
-                                  //   else {
-                                  //     return false;
-                                  //   }
-                                  // };
-
-
 var textForPromptNumberOfCharacters = "Please indicate the number of characters needed for the password (minimum of " + minCharsInPassword + "; maximum of " + maxCharsInPassword + ")";
-
-// var promptNumberOfCharacters = function() {
-//   window.prompt(textForPromptNumberOfCharacters);
-//};
 
 // Assignment code here
 
@@ -121,11 +108,11 @@ var generatePassword = function() {
 // intro statement to user;
   // getPasswordLength;
 
-// XXXXXXXXXXXXXXXXXXXX reset passwordLength to false
-//   while (passwordLength == false) {
-//     passwordLength = window.prompt(textForPromptNumberOfCharacters);
-//     passwordLength = validateEntryNumberWithMinMax(passwordLength, minCharsInPassword, maxCharsInPassword);
-//   }  
+  while (passwordLength == false) {
+    passwordLength = window.prompt(textForPromptNumberOfCharacters);
+    // if return null - cancel
+    passwordLength = validateEntryNumberWithMinMax(passwordLength, minCharsInPassword, maxCharsInPassword);
+  }  
 
 
 // getTypesOfCharacters; TODO: how many include in passwords do we have?
@@ -133,7 +120,7 @@ var firstTime = true;
 while (numberOfTypesOfCharacters == 0) {
   // message to user about what's coming up / error message if they've already tried before
   if (firstTime) {
-    window.alert("Next you will choose types of characters to include in your password; you must include at least one type!");
+    window.alert("Next please choose types of characters to include in your password; you must include at least one type!");
     firstTime = false;
   } else {
     window.alert("Please include at least one type of character in your password!");
@@ -146,6 +133,7 @@ while (numberOfTypesOfCharacters == 0) {
     var typeChoice = false; // this variable is used to repeat the prompt if an invalid entry is given, so it needs to be reset with each for loop
     while (typeChoice == false) {
       typeChoice = window.prompt(textToPromptTypeOfCharacter);
+      // if return null - cancel
       // get validated y or n
       typeChoice = validateEntryYorN(typeChoice);
       // if y, increment number of required chars and update include that type
@@ -167,16 +155,22 @@ for (i = 0; i < passwordSource.length; i++ ) {
   }
 };
 
+// for password length - number of required
+for (i = 0; i < (passwordLength - numberOfTypesOfCharacters); i++ ) {
+  // pull values into the working string from the pull-from string
+  passwordArray.push(getRandomElementFromArray(characterSet));
+};
 debugger;
-  // for password length - number of required
-  for (i = 0; i < (passwordLength - numberOfTypesOfCharacters); i++ ) {
 
-    // pull values into the working string from the pull-from string
-    passwordArray.push(getRandomElementFromArray(characterSet));
-  };
-    // for each type of char
-    // if char is included 
+// for each type of char
+for (i=0; i < passwordSource.length; i++) {
+  // if char is included 
+  if (passwordSource[i].includeInPassword == true) {
     // splice in required chars
+    spliceValueAtRandomIndex(getRandomElementFromArray(passwordSource[i].arrayOfCharacters), passwordArray);
+    
+  };
+}
 
  passwordString = passwordArray.join("");
  return passwordString;
@@ -190,18 +184,11 @@ function writePassword() {
   
   passwordText.value = password;
   
-  //resetVariables(); // added to get ready for next password request and also so that the password is not sitting around in memory for hackers
+  resetVariables(); // added to get ready for next password request and also so that the password is not sitting around in memory for hackers
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-/*
-                            var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-                                  
-                  window.alert("This is an alert! JavaScript is running!");
-                             
-
-    */
    
-   
+   // do you want the same settings as last time?
    
